1507bd7bbff6506d9e1d13cd4646ba9517c14ec9
Thiago Vieira <tpbvieira@gmail.com>
2025-01-24 16:23:37 -0300

    typos

--- src/run_code_review_flow.py
+++ src/run_code_review_flow.py
@@ -18,31 +18,31 @@ class CodeReviewFlow(Flow):
             chatbot_response = requests.post(chatbot_url, headers=headers, json=chatbot_data, verify=False)  # 'verify=False' is equivalent to '--insecure'
             response_json = chatbot_response.json()
             chatbot_message = response_json.get("content", "No content field found")
-            print("# > chatbot_message:", chatbot_message)
+            print("#> chatbot_message:", chatbot_message)
         except RequestException as e:
-            print(f"HTTP Request Error: {e}")
+            print(f"#> HTTP Request Error: {e}")
             return "Error, sorry!"
         except json.JSONDecodeError:
-            print("Response is not in JSON format:", response_json.text)
+            print("#> Response is not in JSON format:", response_json.text)
             return "Error, sorry!"
 
         return chatbot_message
 
     @listen(generate_code)
-    def create_outline(self, chatbot_message):
+    def create_review(self, chatbot_message):
         headers = {"Content-Type": "application/json"}
         code_reviewer_url = "http://localhost:80/analyze-code"
         code_reviewer_data = {"message": chatbot_message}
-        print("# > code_reviewer_data:", code_reviewer_data)
+        print("#> code_reviewer_data:", code_reviewer_data)
 
         try:
             code_reviewer_response = requests.post(code_reviewer_url, headers=headers, json=code_reviewer_data, verify=False)  # 'verify=False' is equivalent to '--insecure'
-            print("# > code_reviewer_response:", code_reviewer_response.text)    
+            print("#> code_reviewer_response:", code_reviewer_response.text)    
         except RequestException as e:
-            print(f"HTTP Request Error: {e}")
+            print(f"#> HTTP Request Error: {e}")
             return "Error, sorry!"
         except json.JSONDecodeError:
-            print(f"Invalid JSON response: {code_reviewer_response.text}")
+            print(f"#> Invalid JSON response: {code_reviewer_response.text}")
             return "Error, sorry!"
         
         return code_reviewer_response.text
@@ -50,6 +50,6 @@ class CodeReviewFlow(Flow):
 async def main():
     flow = CodeReviewFlow()
     result = await flow.kickoff()
-    print(f"Final Outline: {result}")
+    print(f"#> Reviewed code: {result}")
 
 asyncio.run(main())
